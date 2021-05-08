51806aaff98e8a74b5e2422d77e5a0afd7a1fac6
Lisa Xu <lisaxu220@hotmail.com>
2021-05-08 17:55:20 -0400

    created routes, models, controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :bikes, :bookings, :reviews, dependent: :destroy
+  has_many :bikes, foreign_key: :owner_id, dependent: :destroy
+  has_many :bookings, foreign_key: :renter_id, dependent: :destroy
+  has_many :reviews, foreign_key: :renter_id
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # resources :bikes do
-  #   resources :bookings, only: [:new, :create] do 
-  #     resources :reviews, only: [:new, :create]
-  #   end
-  # end
+  resources :bikes do
+    resources :bookings, only: [:new, :create] 
+  end
+
+  resources :bookings, only: [] do
+    resources :reviews, only: [:new, :create]
+  end
 
   resources :reviews, only: [:destroy]
 end
