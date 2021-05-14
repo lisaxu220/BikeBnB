ed720b8d5e50aedb8e8ef05acbbb61b535917076
Ryan Hastings <rhaasti@gmail.com>
2021-05-13 20:12:28 -0400

    added user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+  end
+
+  def destroy
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email,:encrypted_password)
+  end
+
 end
