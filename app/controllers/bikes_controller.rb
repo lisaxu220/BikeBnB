aa464125f9e7a1ad9d15622de48751e1de2238c4
Ryan Hastings <rhaasti@gmail.com>
2021-05-13 23:06:30 -0400

    fixed logout buttong functionality, added new and create for bikes

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,2 +1,23 @@
 class BikesController < ApplicationController
+
+def new
+  @bike = Bike.new
+end
+
+def create
+  @bike = Bike.new(new_bike_params)
+  @bike.owner = current_user
+  if @bike.save
+    redirect_to bikes_path
+  else
+    render :new
+  end
+end
+
+private
+
+def new_bike_params
+  params.require(:bike).permit(:owner_id, :name, :description, :capacity, :price)
+end
+
 end
