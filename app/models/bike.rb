d1d024d80e6ffcc3f2a12140d8a9343946ee0d60
Lisa Xu <lisaxu220@hotmail.com>
2021-05-20 21:05:56 -0400

    render booking page in bikes#show

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -3,10 +3,10 @@ class Bike < ApplicationRecord
   validates :name, :price, :capacity, presence: true
   has_many_attached :photos
 
-  include PgSearch::Model
-  pg_search_scope :search_by_name,
-    against: [ :name],
-    using: {
-      tsearch: { prefix: true }
-    }
+  # include PgSearch::Model
+  # pg_search_scope :search_by_name,
+  #   against: [ :name],
+  #   using: {
+  #     tsearch: { prefix: true }
+  #   }
 end
